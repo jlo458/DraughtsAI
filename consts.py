725ecffd7d5ebcf0bbfa4da6e4c23a00ef602ac3
jlo458 <130149735+jlo458@users.noreply.github.com>
2024-09-01 20:35:47 +0100

    Create consts.py

---
+++ consts.py
@@ -0,0 +1,14 @@
+# File for all constants 
+
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+
+SQUARE_SIZE = WIDTH // COLS
+
+# RGB - change later
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
