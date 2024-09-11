244972e1415a0080bbe8801953d3d7f6cc9feddf
jlo458 <130149735+jlo458@users.noreply.github.com>
2024-09-11 21:18:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,49 @@
-# Main workings of game
+import pygame
+from draughts.board import Board
+from draughts.consts import HEIGHT, WIDTH
+
+FPS = 10
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Draughts AI")
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    board = Board()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        # Game logic
+
+        # Board Maker
+        # board.buildBoard()
+        board.drawAll(window)
+        pygame.display.update()
+
+    pygame.quit()
+
+
+main()
+
+
+# Code for words on right hand side
+# Can be used for leadreboard etc
+# Just need to adjust board WIDTH to 1000
+"""
+pygame.font.init()
+
+titleFont = pygame.font.SysFont("Calibri", 25, True, False)
+        text = titleFont.render("Draughts AI", True, WHITE)
+        window.blit(text, [840, 100])
+"""
+in workings of game
 
 import pygame
 from draughts.consts import BLACK, BLUE, HEIGHT, WIDTH
