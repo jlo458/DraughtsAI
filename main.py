dd394e834aa773f63c419a9d16eaf424fd323cda
jlo458 <130149735+jlo458@users.noreply.github.com>
2024-09-14 22:10:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+# Fix movement bug, may need to switch pieces manually
+
 import pygame
+from draughts import board
 from draughts.board import Board
 from draughts.consts import HEIGHT, WIDTH
 
 FPS = 10
+board = Board()
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Draughts AI")
@@ -15,6 +19,10 @@ def main():
 
     while run:
         clock.tick(FPS)
+
+        piece = board.select_piece(0, 1)
+        #print(piece)
+        board.move(piece, 3, 4)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -42,53 +50,4 @@ pygame.font.init()
 titleFont = pygame.font.SysFont("Calibri", 25, True, False)
         text = titleFont.render("Draughts AI", True, WHITE)
         window.blit(text, [840, 100])
-
-in workings of game
-
-import pygame
-from draughts.consts import BLACK, BLUE, HEIGHT, WIDTH
-
-FPS = 60
-
-window = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Draughts AI")
-
-
-def main():
-    run = True
-    clock = pygame.time.Clock()
-
-    while run:
-        clock.tick(FPS)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-
-        # Game logic
-
-        # Board Maker
-        window.fill(BLACK)
-        for x in range(8):
-            for y in range(8):
-                if (x + y) % 2 == 0:
-                    pygame.draw.rect(window, BLUE, [100 * x, 100 * y, 100, 100])
-
-        pygame.display.flip()
-
-    pygame.quit()
-
-
-main()
-
-
-# Code for words on right hand side
-# Can be used for leadreboard etc 
-# Just need to adjust board WIDTH to 1000
-
-pygame.font.init()
-
-titleFont = pygame.font.SysFont("Calibri", 25, True, False)
-        text = titleFont.render("Draughts AI", True, WHITE)
-        window.blit(text, [840, 100])
-'''
+"""
