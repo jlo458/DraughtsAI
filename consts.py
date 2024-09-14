d9c9e12868ff7867c3acc0a80e36c3ac760adbce
jlo458 <130149735+jlo458@users.noreply.github.com>
2024-09-14 22:08:49 +0100

    Update consts.py

--- consts.py
+++ consts.py
@@ -1,5 +1,3 @@
-# File for all constants 
-
 import pygame
 
 # Dimensions
@@ -12,4 +10,7 @@ SQUARE_SIZE = WIDTH // COLS
 RED = (255, 0, 0)
 BLUE = (10, 150, 140)
 WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+BLACK = (50, 50, 50)
+
+# King sign
+CROWN = pygame.transform.scale(pygame.image.load("draughts/crown.png"), (45, 25))
