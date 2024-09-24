29277846a9f6de36255b0db3546a434f8a23147d
jlo458 <130149735+jlo458@users.noreply.github.com>
2024-09-24 21:38:39 +0100

    Update consts.py

--- consts.py
+++ consts.py
@@ -3,10 +3,9 @@ import pygame
 # Dimensions
 WIDTH, HEIGHT = 800, 800
 ROWS, COLS = 8, 8
-
 SQUARE_SIZE = WIDTH // COLS
 
-# RGB - change later
+# RGB Colours
 RED = (255, 0, 0)
 BLUE = (10, 150, 140)
 WHITE = (255, 255, 255)
