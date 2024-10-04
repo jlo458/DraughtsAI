ef15ecf4bc393b201eb89ad82f13ca277cf2d39e
jlo458 <130149735+jlo458@users.noreply.github.com>
2024-10-04 23:15:05 +0100

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -6,6 +6,7 @@ from copy import deepcopy
 
 import pygame
 
+from draughts import board, game
 from draughts.board import Board
 from draughts.consts import BLACK, WHITE
 from draughts.game import Game
@@ -16,9 +17,35 @@ def miniMax(pos, depth, maxTurn):
         return pos.evaluateFunc(), pos 
     
     if maxTurn: 
-        return max(pos, depth-1, maxTurn = False)
+        maxScore = float("-inf")
+        bestMove = None 
+        for move in getAllMoves(pos, WHITE): # add game input
+            evalScore = miniMax(move, depth-1, False) 
+            maxScore = max(evalScore, maxScore)
+            if maxScore == evalScore: 
+                bestMove = move 
     
     else: 
-        return min(pos, depth-1, maxTurn = True)
+        minScore = float("inf")
+        bestMove = None 
+        for move in getAllMoves(pos, BLACK): # add game input
+            evalScore = miniMax(move, depth-1, True) 
+            minScore = min(evalScore, maxScore)
+            if minScore == evalScore: 
+                bestMove = move 
+    
+    
+
+def getAllMoves(pos, col): 
+    moves = []
+    for piece in board.getAllPieces(col): 
+        validMoves = game.getValidMoves(piece)
+        for move in validMoves:
+            testBoard = deepcopy(pos)
+            moves.append([simMove(piece, move, testBoard), piece])
+
+def simMove(self, piece, move, testBoard): 
+    testBoard.move(piece, move[0], move[1])
+    return testBoard
     
 
