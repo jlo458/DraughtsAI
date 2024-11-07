10a230bd226b88397afb77fa7ec0b4ac791c45df
jlo458 <130149735+jlo458@users.noreply.github.com>
2024-11-07 19:00:46 +0000

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -7,38 +7,46 @@ from copy import deepcopy
 import pygame
 
 from draughts import board, game
-
-#from draughts.board import Board
 from draughts.consts import BLACK, WHITE
 
-#from draughts.game import Game
 
-
-def miniMax(pos, depth, maxTurn): 
+def miniMax(pos, depth, maxTurn, alpha, beta): 
     if depth == 0 or pos.checkWinner():
         return pos.evaluateFunc(), pos # Change if pos is irrevevant 
     
     if maxTurn: 
-        maxScore = float("-inf")
+        maxEval = float("-inf")
         bestMove = None 
-        for move in getAllMoves(pos, WHITE): # add game input
-            evalScore = miniMax(move, depth-1, False)[0] 
-            maxScore = max(evalScore, maxScore)
-            if maxScore == evalScore: 
+        for move in getAllMoves(pos, WHITE): 
+            evalScore = miniMax(move, depth-1, False, alpha, beta)[0] 
+            maxEval = max(evalScore, maxEval)
+
+            alpha = max(maxEval, alpha) 
+            if alpha >= beta:
+                #print(f"Alphabeta pruning depth {depth}") 
+                break
+
+            if maxEval == evalScore: 
                 bestMove = move 
 
-        return maxScore, bestMove
+        return maxEval, bestMove
     
     else: 
-        minScore = float("inf")
+        minEval = float("inf")
         bestMove = None 
         for move in getAllMoves(pos, BLACK): # add game input
-            evalScore, _ = miniMax(move, depth-1, True)
-            minScore = min(evalScore, minScore)
-            if minScore == evalScore: 
+            evalScore, _ = miniMax(move, depth-1, True, alpha, beta)
+            minEval = min(evalScore, minEval)
+
+            beta = min(minEval, beta) 
+            if alpha >= beta: 
+                #print(f"Alphabeta pruning depth {depth}") 
+                break
+
+            if minEval == evalScore: 
                 bestMove = move 
 
-        return minScore, bestMove
+        return minEval, bestMove
     
 
 def getAllMoves(pos, colour): 
@@ -69,5 +77,24 @@ def simMove(piece, move, testBoard):
                 testBoard = simMove(piece, move, testBoard)
 
     return testBoard
+    
+
+'''
+reMove = False
+        if self.selected and (row, col) in self.validMoves:
+            oldRow = self.selected.row 
+            oldCol = self.selected.col
+            self.board.move(self.selected, row, col)
+            if self.selected.row == oldRow-2 or self.selected.row == oldRow+2:
+                self.board.removePiece((oldRow+self.selected.row)//2, (oldCol+self.selected.col)//2)
+                moves = self.board.checkDouble(self.selected, self.selected.colour)
+                self.validMoves = []
+                for move in moves: 
+                    if move is not None: 
+                        reMove = True
+                        self.validMoves.append(move)
+
+            if not reMove:
+                self.changeTurn()'''
     
 
