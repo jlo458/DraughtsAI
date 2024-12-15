d56f05bb7419ff412f2deee9e256794fca9b770a
jlo458 <130149735+jlo458@users.noreply.github.com>
2024-12-15 21:36:56 +0000

    Update altNetwork.py

--- altNetwork.py
+++ altNetwork.py
@@ -1,15 +1,17 @@
-imporimport random
+import random
 import time
 from collections import deque
-from tqdm import tqdm
-import numpy
+from pickle import TRUE
+from tabnanny import verbose
 
+import numpy
 import numpy as np
 import tensorflow as ts
 from keras._tf_keras.keras.callbacks import TensorBoard
 from keras._tf_keras.keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D
 from keras._tf_keras.keras.models import Sequential
 from keras._tf_keras.keras.optimizers import Adam
+from tqdm import tqdm
 
 from deepQ.tensorBoardMod import ModifiedTensorBoard
 from draughts import board
@@ -35,20 +37,27 @@ EPSILON_DECAY = 0.99975
 MIN_EPSILON = 0.001 
 
 class draughtEnv(): # You will have input board, don't remake it, just swap pieces for 0, 1
-    def __init__(self, pos, go) -> None:
-        self.board = pos
+    def __init__(self, pos=None) -> None:
+        self.board = pos or Board.buildBoard()
         self.startGame(pos)
 
     def reset(self): 
         pos = Board.buildBoard()
         self.startGame(pos)  
+        return self.vectorBoard  # check
 
     def startGame(self, pos): 
         self.vectorBoard = self.vectorise(pos)
+        self.validMoves = None
+        self.action_map = None
         self.go = 2 
         self.reward = 0
         self.done = False 
         self.episodeNum = 1
+
+    def findValidMoves(self, board): 
+        self.validMoves = self.board.possibleMoves()
+        return self.validMoves
 
     def vectorise(self, board):
         for row in board: 
@@ -69,10 +78,14 @@ class draughtEnv(): # You will have input board, don't remake it, just swap piec
                         else: 
                             board[row][col] = 4
 
+        return board
+
     def step(self, action, go):
         done = False 
         bl, bk, wl, wk = self.board.takeInfo()
-        self.board = self.board.move(action)
+
+        theMove = self.validMoves[action]
+        self.board = self.board.move(theMove)
 
         self.episodeNum += 1 
 
@@ -84,12 +97,23 @@ class draughtEnv(): # You will have input board, don't remake it, just swap piec
 
         # make reward 
         miniWeight = 1
-        reward = ((wla - wl)+(bl - bla) + (wka - wk)+(bk - bka))*whiteGo*miniWeight
+        reward += ((wla - wl)+(bl - bla))*whiteGo*miniWeight
+        reward += ((wka - wk)+(bk - bka))*whiteGo*miniWeight*2
 
-        if self.board.checkWinner(): 
+        if self.board.checkWinner():
+            if self.board.checkWinner()[1] == go:
+                reward += 1000 
+                done = True
+
+            if self.board.checkWinner()[1] != go:
+                reward -= 1000 
+                done = True
+
+        if len(self.validMoves) == 0:
+            reward -= 900
             done = True
 
-        return reward, done
+        return self.vectorise(self.board), reward, done
 
 class DQN_Agent: 
     def __init__(self, stateShape, actionSize, bufferSize = 2000):
@@ -110,14 +134,13 @@ class DQN_Agent:
         model.compile(optmiser=Adam(learning_rate=0.001), loss='mse')
         return model 
     
-    def act(self, state): 
+    def act(self, state, valMoves): 
         if np.random.random() < self.epsilon: 
-            pass 
-            # Random Action 
-            # Return np.random.randint(self.action_size)
-
-        q_values = self.model.predict(state[np.newaxis])
-        return numpy.argmax(q_values)
+            action_idx = np.random.randint(len(valMoves))
+        else:
+            q_values = self.model.predict(state[np.newaxis], verbose=0)
+            action_idx = np.argmax(q_values[:len(valMoves)])  # Restrict to valid moves
+        return self.action_map[action_idx]  # Convert to actual move
     
     def train(self, batchSize): 
         if self.memorySize < batchSize: 
@@ -126,12 +149,13 @@ class DQN_Agent:
         batch = self.memory.sample(batchSize)
         states, actions, rewards, nextStates, dones = map(np.array, zip(*batch))
 
-        target_qs = self.model.predict(states)
+        current_qs = self.model.predict(states)
         next_qs = self.target_model.predict(nextStates)
         for i in range(batchSize):
-            target_qs[i, actions[i]] = rewards[i] + (1 - dones[i]) * self.gamma * np.max(next_qs[i])
+            max_future_q = np.max(next_qs[i]) if not dones[i] else 0
+            current_qs[i, actions[i]] = rewards[i] + self.gamma * max_future_q
 
-        self.model.fit(states, target_qs, verbose=0)
+        self.model.fit(states, current_qs, verbose=0)
 
         self.epsilon = max(MIN_EPSILON, self.epsilon * EPSILON_DECAY)
 
@@ -166,7 +190,10 @@ for episode in range(EPISODES):
     done = False 
 
     while not done: 
-        action = agent.act(state)
+        validMoves = env.findValidMoves()
+        env.action_map = {i: move for i, move in enumerate(validMoves)}
+
+        action = agent.act(state, env.validMoves)
         nextState, reward, done, _ = env.step(action)
         agent.memory.add((state, action, reward, nextState, done))
         agent.train(batchSize)
@@ -175,90 +202,33 @@ for episode in range(EPISODES):
 
 agent.updateTargetModel()
 print(f"Episode {episode}, Total Reward: {totalReward}")
-t random
-import time
-from collections import deque
-from networkx import write_weighted_edgelist
-from tqdm import tqdm
 
-import numpy as np
-import tensorflow as ts
-from keras._tf_keras.keras.callbacks import TensorBoard
-from keras._tf_keras.keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D
-from keras._tf_keras.keras.models import Sequential
-from keras._tf_keras.keras.optimizers import Adam
+# Proper reward system 
 
-from deepQ.tensorBoardMod import ModifiedTensorBoard
-from draughts import board
-from draughts.board import Board
-from draughts.consts import WHITE
-from draughts.piece import Piece
-
-MODEL_NAME = "draughts-dqn"
-
-REPLAY_MEMORY_SIZE = 50_000
-MIN_REPLAY_MEMORY_SIZE = 1_000
-MINIBATCH_SIZE = 64
-UPDATE_TARGET_VALUE = 100
-
-GAMMA = 0.99 # Discount factor - higher = future rewards are more important than if it were 0.9
-
-# REWARD ...
-
-EPISODES = 20_000 
-
-epsilon = 1 
-EPSILON_DECAY = 0.99975 
-MIN_EPSILON = 0.001 
-
-class draughtEnv(): # You will have input board, don't remake it, just swap pieces for 0, 1
-    def __init__(self, pos, go) -> None:
-        self.board = pos
-        self.startGame(pos)
-
-    def reset(self): 
-        pos = Board.buildBoard()
-        self.startGame(pos)  
-
-    def startGame(self, pos): 
-        self.vectorBoard = self.vectorise(pos)
-        self.go = 2 
-        self.reward = 0
-        self.done = False 
-        self.episodeNum = 1
-
-    def vectorise(self, board):
-        for row in board: 
-            for col in range(len(row)): 
-                piece = row[col]
-                if isinstance(piece, Piece): 
-                    if piece.colour == WHITE: 
-                        if piece.king: 
-                            board[row][col] = 1
-
-                        else: 
-                            board[row][col] = 3
-
-                    else: 
-                        if piece.king: 
-                            board[row][col] = 2
-
-                        else: 
-                            board[row][col] = 4
-
-    def step(self, action, go): 
-        # reward
-        bl, bk, wl, wk = self.board.takeInfo()
-        self.board = self.board.move(action)
-
-        self.episodeNum += 1 
-
+'''
+class draughtEnv:
+    ...
+    def step(self, action):
+        ...
+        # Compute changes in piece counts
+        reward = 0
         bla, bka, wla, wka = self.board.takeInfo()
 
-        whiteGo = -1
-        if go == 1: 
-            whiteGo = 1
+        # Reward based on capturing pieces
+        reward += (wl - wla) - (bl - bla)  # Difference in lost pieces
+        reward += 2 * ((wk - wka) - (bk - bka))  # Higher weight for kings
 
-        # make reward 
+        # Reward for winning or penalty for losing
+        if self.board.checkWinner() == 1:  # White wins
+            reward += 100
+            done = True
+        elif self.board.checkWinner() == -1:  # Black wins
+            reward -= 100
+            done = True
+        else:
+            done = False
 
-        reward = ...
+        if len(self.validMoves) == 0:
+            reward -= 50
+        
+        return reward, done'''
